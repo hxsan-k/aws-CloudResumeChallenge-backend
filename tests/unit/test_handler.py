cc30ddffc2545b0bc0f0bdb351bdc74d7508d778
Hasan Kamran <hasan8812011@hotmail.co.uk>
2025-06-21 23:44:12 +0100

    Fix test: mock DynamoDB and use local test event

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,5 +1,7 @@
-import json
+import boto3
 import pytest
+import json
+from moto import mock_dynamodb
 from app import lambda_handler
 
 @pytest.fixture()
@@ -10,9 +12,21 @@ def apigw_event():
         "path": "/hello"
     }
 
+@mock_dynamodb()
 def test_lambda_handler(apigw_event):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.create_table(
+        TableName='ViewCounterTable',
+        KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
+        AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'S'}],
+        ProvisionedThroughput={'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1}
+    )
+
+    table.put_item(Item={'id': 'counter', 'views': 0})
+
     response = lambda_handler(apigw_event, "")
-    data = json.loads(response["body"])
+    body = json.loads(response["body"])
+
     assert response["statusCode"] == 200
-    assert "views" in data
-    assert isinstance(data["views"], int)
+    assert "views" in body
+    assert isinstance(body["views"], int)
