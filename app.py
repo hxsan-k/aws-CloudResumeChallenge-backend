b23f07ef25a7bc6bb859b045a52e9ff4a20542d8
Hasan Kamran <hasan8812011@hotmail.co.uk>
2025-06-23 01:21:05 +0100

    Fix Lambda for testing by moving table declaration inside handler

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import json
 import boto3
+import os
 from botocore.exceptions import ClientError
 from decimal import Decimal
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('ViewCounterTable')
-
 def lambda_handler(event, context):
+    table_name = os.environ.get("TABLE_NAME", "ViewCounterTable")
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
     try:
         response = table.get_item(Key={'id': 'counter'})
         count = response.get('Item', {}).get('views', 0)
 
-        # DynamoDB returns Decimal, convert it to int
         if isinstance(count, Decimal):
             count = int(count)
 
@@ -29,5 +30,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({'error': 'Something went wrong'})
         }
-# xyz
-# Trigger workflow
